c7ae0a05ed60f27e8e1b3720d6ae44f961e6b1a4
Piotr Czaja <czaja_piotr@o2.pl>
2024-03-19 19:00:29 +0100

    python3.12 update - regex

--- pakietdemo/wykonaj.py
+++ pakietdemo/wykonaj.py
@@ -5,10 +5,10 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 version = re.search(
-    '^__version__\s*=\s*"(.*)"', open(os.path.join(here, "__init__.py")).read(), re.M
+    '^__version__\\s*=\\s*"(.*)"', open(os.path.join(here, "__init__.py")).read(), re.M
 ).group(1)
 release = re.search(
-    '^__release__\s*=\s*"(.*)"', open(os.path.join(here, "__init__.py")).read(), re.M
+    '^__release__\\s*=\\s*"(.*)"', open(os.path.join(here, "__init__.py")).read(), re.M
 ).group(1)
 __version__ = version
 __release__ = release
